f57ab355ab07aea03663f19b4c950273d8df6277
fibletype <fibletyp@gmail.com>
2020-11-05 14:32:45 +0300

    Done

--- guess_num.py
+++ guess_num.py
@@ -14,5 +14,33 @@
 Тебе может понадобится модуль random, цикл while и ветвления
 """
 
+import random as rnd
+
+
+def read():
+    while True:
+        inp = input('Guess number form 0 to 10^6 ')
+        if inp != "exit" and inp != '':
+            if not inp.isdigit():
+                print('Error: not a number')
+                continue
+            if not (0 <= int(inp) <= 1000000):
+                print('Error: not in range')
+                continue
+            else:
+                return int(inp)
+        else:
+            print('exit')
+            exit()
+
+
 if __name__ == '__main__':
-    pass
+    desired_num = rnd.randint(0, 1000000)
+    inp = read()
+    while inp != desired_num:
+        if inp > desired_num:
+            print("Your number is bigger")
+        else:
+            print("Your number is smaller")
+        inp = read()
+    print(f"You are right! Number = {desired_num}")
