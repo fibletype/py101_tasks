37cd81e9713a3a12026fb45ed2d4d3e70eb10c01
fibletype <fibletyp@gmail.com>
2020-11-05 16:55:45 +0300

    Done

--- password.py
+++ password.py
@@ -5,6 +5,12 @@
 Сложным считается пароль, состоящий как минимум из 8-ми символов,
 включая цифры и алфавитные символы
 """
+import re
 
 if __name__ == '__main__':
-    pass
+    password = input('Enter password: ')
+    if re.search('[A-Z]', password) and re.search('[a-z]', password) and \
+        re.search('[0-9]', password) and len(password) >= 8:
+        print('Strong password')
+    else:
+        print('Weak password')
