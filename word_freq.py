f993fe7846a7bbcf8a3023b2b0c8e970b7d83590
fibletype <fibletyp@gmail.com>
2020-11-06 15:25:06 +0300

    Done

--- word_freq.py
+++ word_freq.py
@@ -8,5 +8,23 @@
 Тебе может понадобится модуль os, модуль argparse, цикл и словарь
 """
 
+import os
+import nltk
+import sys
+from collections import Counter
+
 if __name__ == '__main__':
-    pass
+    stop_words = set(nltk.stopwords.words('english'))
+    if len(sys.argv) > 1:
+        if os.path.isfile(file := sys.argv[1]):
+            with open(file, encoding='UTF-8') as file_name:
+                words = file_name.read().split()
+            word_tokens = nltk.word_tokenize(words)
+            not_stop_words = []
+            for i in word_tokens:
+                if i not in stop_words:
+                    not_stop_words.append(i)
+            sorted_words = str(Counter(not_stop_words).most_common(100))
+            print(sorted_words)
+        else:
+            print('Error: File doesnt exists')
